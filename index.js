2bba41322fa97c2f40ac2b7553e49cef7fe61bf2
1sanyo <sanyo.adonai@student.moringaschool.com>
2022-08-14 15:26:46 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,5 @@
+whisper(string)
+receives one argument and returns it in all lowercase:
+ReferenceError: whisper is not defined
+(test/index-test.js:10:5)
+at processImmediate (internal/timers.js:456:21)
